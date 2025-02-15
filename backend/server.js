206c6be31a9e4c0ab6b5162d803b1bdc39a0634c
useris <dominykas.kruminas@gmail.com>
2025-02-15 18:12:57 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,12 @@ mongoose.connect(
   }
 );
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
+
 // Use routes
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
