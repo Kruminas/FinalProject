f58ac57256fdafcef11a4d0353d7ef803e6e2a11
useris <dominykas.kruminas@gmail.com>
2025-02-15 19:25:19 +0200

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+
+// Auth
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
+// Admin
+import UserManagement from './components/Admin/UserManagement';
+
+// Templates
 import TemplateList from './components/Templates/TemplateList';
 import CreateTemplate from './components/Templates/CreateTemplate';
 import FillForm from './components/Templates/FillForm';
-import TemplatePage from './components/Templates/TemplatePage';
 import SearchResults from './components/Search/SearchResults';
-import FormList from './components/Forms/FormList';
-import FormPage from './components/Forms/FormPage';
-import UserManagement from './components/Admin/UserManagement';
 
-export default function App() {
+// Forms
+import FormList from './components/Forms/FormList';
+import TemplatePage from './components/Templates/TemplatePage';
+import FormPage from './components/Forms/FormPage';
+
+function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<TemplateList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        {/* Templates */}
+        <Route path="/templates" element={<TemplateList />} />
         <Route path="/templates/create" element={<CreateTemplate />} />
         <Route path="/templates/:id/fill" element={<FillForm />} />
         <Route path="/templates/:id/edit" element={<TemplatePage />} />
         <Route path="/search" element={<SearchResults />} />
+
+        {/* Admin - view all forms, manage users */}
         <Route path="/forms" element={<FormList />} />
         <Route path="/forms/:id" element={<FormPage />} />
         <Route path="/admin/users" element={<UserManagement />} />
@@ -28,3 +45,5 @@ export default function App() {
     </Router>
   );
 }
+
+export default App;
