a1906c3589257a1aff4cab9d66afd7b5304abcfe
useris <dominykas.kruminas@gmail.com>
2025-02-16 03:28:19 +0200

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,9 +17,10 @@ export default function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<TemplateList />} />
+        <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/templates" element={<TemplateList />} />
         <Route path="/templates/create" element={<CreateTemplate />} />
         <Route path="/templates/:id/fill" element={<FillForm />} />
         <Route path="/templates/:id/edit" element={<TemplatePage />} />
