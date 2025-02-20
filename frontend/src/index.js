2accf08b4761b8a6639e533d091b34aa372716ab
useris <dominykas.kruminas@gmail.com>
2025-02-20 20:54:59 +0200

    finalcommit?

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,7 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
