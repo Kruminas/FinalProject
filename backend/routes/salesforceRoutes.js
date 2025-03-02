9a34acddcad1133bed8c3b7049d35c04535f446d
useris <dominykas.kruminas@gmail.com>
2025-03-03 01:35:54 +0200

    test

--- backend/routes/salesforceRoutes.js
+++ backend/routes/salesforceRoutes.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const authenticateToken = require('../middlewares/authMiddleware');
 const axios = require('axios');
 
 async function getSalesforceToken() {
-  const tokenUrl = 'https://login.salesforce.com/services/oauth2/token';
+  const tokenUrl = 'https://login.salesforce.com/services/oauth2/token'; 
+
   const params = new URLSearchParams();
   params.append('grant_type', 'password');
-  params.append('client_id', procces.env.SF_CONSUMER_KEY);
-  params.append('client_secret', procces.env.SF_CONSUMER_SECRET);
+  params.append('client_id', process.env.SF_CONSUMER_KEY);
+  params.append('client_secret', process.env.SF_CONSUMER_SECRET);
   params.append('username', process.env.SF_USERNAME);
   params.append('password', process.env.SF_PASSWORD);
 
   try {
     const response = await axios.post(tokenUrl, params);
-    console.log('Salesforce token response:', response.data);
-    return { accessToken: response.data.access_token, instanceUrl: response.data.instance_url };
+    return {
+      accessToken: response.data.access_token,
+      instanceUrl: response.data.instance_url
+    };
   } catch (error) {
     console.error('Error obtaining Salesforce token:', error.response?.data || error.message);
     throw error;
   }
 }
 
-router.post('/create', authenticateToken, async (req, res) => {
+
+router.post('/create', async (req, res) => {
   try {
     const { sfAccountName, sfContactName, sfContactEmail } = req.body;
     const tokenData = await getSalesforceToken();
@@ -31,14 +34,18 @@ router.post('/create', authenticateToken, async (req, res) => {
 
     const accountResp = await axios.post(
       `${instanceUrl}/services/data/v57.0/sobjects/Account`,
-      { Name: sfAccountName },
+      { Name: sfAccountName }, 
       { headers: { Authorization: `Bearer ${accessToken}` } }
     );
     const accountId = accountResp.data.id;
 
     const contactResp = await axios.post(
       `${instanceUrl}/services/data/v57.0/sobjects/Contact`,
-      { LastName: sfContactName, Email: sfContactEmail, AccountId: accountId },
+      { 
+        LastName: sfContactName,
+        Email: sfContactEmail,
+        AccountId: accountId 
+      }, 
       { headers: { Authorization: `Bearer ${accessToken}` } }
     );
 
