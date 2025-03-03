e25205c168c5f22a903eae8f78ec943a8ff9f0b0
useris <dominykas.kruminas@gmail.com>
2025-03-03 20:23:24 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
 const authRoutes = require('./routes/authRoutes');
 const templateRoutes = require('./routes/templateRoutes');
 const formRoutes = require('./routes/formRoutes');
